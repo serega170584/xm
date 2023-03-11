b47b333551f7af57f13b10a63aea0988eaea7530
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-03-11 16:26:56 +0300

    company entity adding

---
+++ project/migrations/Version20230311132339.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230311132339 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE company_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE company (id INT NOT NULL, company_name VARCHAR(255) NOT NULL, financial_status VARCHAR(1) NOT NULL, market_category VARCHAR(1) NOT NULL, round_lot_size NUMERIC(5, 2) NOT NULL, security_name VARCHAR(255) NOT NULL, symbol VARCHAR(4) NOT NULL, test_issue VARCHAR(1) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE company_id_seq CASCADE');
+        $this->addSql('DROP TABLE company');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
---
+++ project/src/Entity/Company.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompanyRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CompanyRepository::class)]
+class Company
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $companyName = null;
+
+    #[ORM\Column(length: 1)]
+    private ?string $financialStatus = null;
+
+    #[ORM\Column(length: 1)]
+    private ?string $marketCategory = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $roundLotSize = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $securityName = null;
+
+    #[ORM\Column(length: 4)]
+    private ?string $symbol = null;
+
+    #[ORM\Column(length: 1)]
+    private ?string $testIssue = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCompanyName(): ?string
+    {
+        return $this->companyName;
+    }
+
+    public function setCompanyName(string $companyName): self
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+
+    public function getFinancialStatus(): ?string
+    {
+        return $this->financialStatus;
+    }
+
+    public function setFinancialStatus(string $financialStatus): self
+    {
+        $this->financialStatus = $financialStatus;
+
+        return $this;
+    }
+
+    public function getMarketCategory(): ?string
+    {
+        return $this->marketCategory;
+    }
+
+    public function setMarketCategory(string $marketCategory): self
+    {
+        $this->marketCategory = $marketCategory;
+
+        return $this;
+    }
+
+    public function getRoundLotSize(): ?string
+    {
+        return $this->roundLotSize;
+    }
+
+    public function setRoundLotSize(string $roundLotSize): self
+    {
+        $this->roundLotSize = $roundLotSize;
+
+        return $this;
+    }
+
+    public function getSecurityName(): ?string
+    {
+        return $this->securityName;
+    }
+
+    public function setSecurityName(string $securityName): self
+    {
+        $this->securityName = $securityName;
+
+        return $this;
+    }
+
+    public function getSymbol(): ?string
+    {
+        return $this->symbol;
+    }
+
+    public function setSymbol(string $symbol): self
+    {
+        $this->symbol = $symbol;
+
+        return $this;
+    }
+
+    public function getTestIssue(): ?string
+    {
+        return $this->testIssue;
+    }
+
+    public function setTestIssue(string $testIssue): self
+    {
+        $this->testIssue = $testIssue;
+
+        return $this;
+    }
+}
